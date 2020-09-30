d11c2ec8cc11291579ff7f8cf05e682a58a7c409
kkidd2020 <70182581+kkidd2020@users.noreply.github.com>
2020-09-30 16:13:36 -0500

    finished product
    finished product

---
+++ forms/while/while.js
@@ -0,0 +1,10 @@
+
+let loops = prompt("Enter number of times you want to run the loop")
+parseInt(loops);
+i = 1
+while (i <= loops) {
+  console.log(`This loop is iteration ${i}.`);
+  i++;
+}
+
+alert("The loop is now done");
